ffe7b5de65e8e6d1e6a0c05b06c7fa6ce89a7b67
Seth Hendrick <seth@shendrick.net>
2021-02-07 15:19:25 -0500

    Added DockerLogin helper function.

---
+++ vars/X13DockerLogin.groovy
@@ -0,0 +1,37 @@
+//
+//          Copyright Seth Hendrick 2020-2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
+
+/// \brief - Logs into docker using the Docker Login command
+///          with the supplied username/credentials
+///          id.  This can either be a docker password
+///          or an authorization token.
+/// \param credsId - Required.  The credentials to use.
+def call( Map args )
+{
+    X13NotNull( args["credsId"], "credsId" );
+
+    String passwordVar = "X13_DOCKER_LOGIN_PASSWORD";
+
+    withCredentials(
+        [usernamePassword(
+            credentialsId: args["credsId"],
+            usernameVariable: "X13_DOCKER_LOGIN_USERNAME",
+            passwordVarialbe: passwordVar
+        )]
+    )
+    {
+        String dockerCommand = "docker login --username ${X13_DOCKER_LOGIN_USERNAME} --password-stdin";
+        if( isUnix() )
+        {
+            sh 'echo "$X13_DOCKER_LOGIN_PASSWORD" | ' + dockerCommand;
+        }
+        else
+        {
+            bat "echo %X13_DOCKER_LOGIN_PASSWORD% | ${dockerCommand}"
+        }
+    }
+}
